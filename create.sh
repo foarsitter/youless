ea19f72a9d6bf4fbe42241b018bb2a6fb6430228
Jelmer Draaijer <info@jelmert.nl>
2022-10-02 11:48:16 +0200

    chore: testing action

--- create.sh
+++ create.sh
@@ -3,8 +3,8 @@ docker run --rm \
   -v ${PWD}:/local openapitools/openapi-generator-cli generate \
    --git-repo-id youless \
    --git-user-id foarsitter \
-  -i /local/Jelmert-Engineering-Youless-0.1.3-swagger.json \
+  -i /local/Jelmert-Engineering-Youless-0.1.5-swagger.json \
   -g go \
   -o /output \
   --additional-properties=packageName=youless \
-  --additional-properties=packageVersion=0.1.3
+  --additional-properties=packageVersion=0.1.5
